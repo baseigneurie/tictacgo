24808c71f650a0400cdb42ebc5959ab8d2f79d73
baseigneurie <bseig@outlook.com>
2016-01-16 11:18:34 -0600

    Starting over

--- main.go
+++ main.go
@@ -1,29 +1,41 @@
 package main
 
 import (
-	"io"
+	"fmt"
 	"log"
-	"net/http"
 
-	"github.com/gorilla/mux"
+	"github.com/jroimartin/gocui"
 )
 
-func main() {
-	r := mux.NewRouter()
-	r.HandleFunc("/", EmptyResponse)
-	r.HandleFunc("/game", GameInit)
-	http.Handle("/", r)
-	err := http.ListenAndServe(":3000", nil)
-	if err != nil {
-		log.Fatal("ListenAndServe: ", err)
+func layout(g *gocui.Gui) error {
+	//maxX, maxY := g.Size()
+	if v, err := g.SetView("corner", 0, 0, 10, 10); err != nil {
+		if err != gocui.ErrorUnkView {
+			return err
+		}
+		v.Wrap = true
+		fmt.Fprintln(v, "X       X X     X   X   X     X X       X")
 	}
+	return nil
 }
 
-func GameInit(w http.ResponseWriter, req *http.Request) {
-	mode = req.URL.Query().Get("mode")
-	io.WriteString(w, mode)
+func quit(g *gocui.Gui, v *gocui.View) error {
+	return gocui.Quit
 }
 
-func EmptyResponse(w http.ResponseWriter, req *http.Request) {
-	io.WriteString(w, "Helo World!")
+func main() {
+	var err error
+	g := gocui.NewGui()
+	if err := g.Init(); err != nil {
+		log.Panicln(err)
+	}
+	defer g.Close()
+	g.SetLayout(layout)
+	if err := g.SetKeybinding("", gocui.KeyCtrlC, gocui.ModNone, quit); err != nil {
+		log.Panicln(err)
+	}
+	err = g.MainLoop()
+	if err != nil && err != gocui.Quit {
+		log.Panicln(err)
+	}
 }
