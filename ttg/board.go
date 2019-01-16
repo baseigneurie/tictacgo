81679eb80f393070ec78c74994c950f09a1a9810
baseigneurie <baseigneurie@gmail.com>
2019-01-16 16:33:11 -0600

    Board generation almost complete

--- ttg/board.go
+++ ttg/board.go
@@ -1,28 +1,49 @@
 package ttg
 
-import "fmt"
+import (
+	"fmt"
+	"strconv"
+
+	"github.com/fatih/color"
+)
 
 var Plays [3][3]string
+var boardLayout string
 
 func DrawBoard() {
 
+	Plays[0][0] = "x"
 	Plays[0][1] = "x"
-	fmt.Println(Plays)
-	CheckPlays()
+	Plays[0][2] = "x"
+	Plays[2][2] = "o"
+
+	// boardLayout = "Total Moves: XX   "
+	// boardLayout += "Current Player: X\n\n"
+
+	head := "Total Moves: XX   "
+	head += "Current Player: X\n\n"
+
+	boardLayout = loadBoard(boardLayout)
+	d := color.New(color.FgRed, color.Bold)
+
+	d.Printf(head)
+	fmt.Printf(boardLayout)
 }
 
-func CheckPlays() {
+func loadBoard(str string) string {
 	p := &Plays
-	str := ``
+	str += "   A | B | C \n\n"
 
 	for i, row := range p {
+		str += strconv.Itoa(i+1) + " "
+
 		for j, col := range row {
 			if col == "o" {
-				str += "O"
+				str += " O "
 			} else if col == "x" {
-				str += "X"
+				str += " X "
 			} else {
-				str += " "
+				str += "   "
 			}
 
 			if j == 2 {
@@ -30,13 +51,12 @@ func CheckPlays() {
 			} else {
 				str += "|"
 			}
-			fmt.Println(str)
 		}
 
 		if i < 2 {
-			str += "---+---+---\n"
+			str += "  ---+---+---\n"
 		}
 	}
 
-	fmt.Println(str)
+	return str
 }
