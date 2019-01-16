1848fd1c4a5263bcfd11be8c5aa63eca447f2e33
Broc Seigneurie <baseigneurie@gmail.com>
2019-01-15 21:55:50 -0600

    built marker arrays

--- main.go
+++ main.go
@@ -2,40 +2,17 @@ package main
 
 import (
 	"fmt"
-	"log"
 
-	"github.com/jroimartin/gocui"
+	"github.com/baseigneurie/tictacgo/ttg"
 )
 
-func layout(g *gocui.Gui) error {
-	//maxX, maxY := g.Size()
-	if v, err := g.SetView("corner", 0, 0, 10, 10); err != nil {
-		if err != gocui.ErrorUnkView {
-			return err
-		}
-		v.Wrap = true
-		fmt.Fprintln(v, "X       X X     X   X   X     X X       X")
-	}
-	return nil
-}
-
-func quit(g *gocui.Gui, v *gocui.View) error {
-	return gocui.Quit
-}
-
 func main() {
-	var err error
-	g := gocui.NewGui()
-	if err := g.Init(); err != nil {
-		log.Panicln(err)
+	x, o := ttg.GetMarkers()
+	for _, n := range x {
+		fmt.Println(n)
 	}
-	defer g.Close()
-	g.SetLayout(layout)
-	if err := g.SetKeybinding("", gocui.KeyCtrlC, gocui.ModNone, quit); err != nil {
-		log.Panicln(err)
-	}
-	err = g.MainLoop()
-	if err != nil && err != gocui.Quit {
-		log.Panicln(err)
+
+	for _, n := range o {
+		fmt.Println(n)
 	}
 }
