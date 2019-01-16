76615eff1148d94fd75ad0097226cfc3c1c0017d
Broc Seigneurie <baseigneurie@gmail.com>
2019-01-15 22:32:03 -0600

    Stuck for now

--- main.go
+++ main.go
@@ -1,18 +1,9 @@
 package main
 
 import (
-	"fmt"
-
 	"github.com/baseigneurie/tictacgo/ttg"
 )
 
 func main() {
-	x, o := ttg.GetMarkers()
-	for _, n := range x {
-		fmt.Println(n)
-	}
-
-	for _, n := range o {
-		fmt.Println(n)
-	}
+	ttg.DrawBoard()
 }
